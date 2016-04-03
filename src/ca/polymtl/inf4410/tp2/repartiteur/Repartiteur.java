64787f9114cbda0deb023bdc6ec6067ce30944e0
Maxime Perusse <Maxime Perusse>
2016-04-03 04:58:08 -0400

    java is dumb sometimes

--- src/ca/polymtl/inf4410/tp2/repartiteur/Repartiteur.java
+++ src/ca/polymtl/inf4410/tp2/repartiteur/Repartiteur.java
@@ -135,9 +135,8 @@ public class Repartiteur implements Observer
 		if(mIsModeSecurise) 
 		{
 			// split the operations in different tasks (group of operations) to be executed on threads
-			int nOperationByTask = (int) Math.ceil(mOperations.size() / mCalculateurs.size());
-			System.out.println("#operations = " + mOperations.size() + ", #calculateurs = " + mCalculateurs.size() );
-			System.out.println("#opbytask = " + nOperationByTask);
+			double dOperationByTask = mOperations.size() / mCalculateurs.size();
+			int nOperationByTask = (int) Math.ceil(dOperationByTask);
 			List<List<Operation>> list_operations = chunk(mOperations, nOperationByTask);
 			
 			if (SHOW_DEBUG_INFO)
